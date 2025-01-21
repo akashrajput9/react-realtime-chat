70d316ce7ecdb192e57c004f1ed8fc7426e70ced
akashinovio <akash@inoviotech.com>
2025-01-21 23:41:31 +0500

    changes

--- src/config.js
+++ src/config.js
@@ -61,6 +61,7 @@ export const defaultLang = allLangs[0]; // English
 // export const BASE_API = "http://realtime-live-chat.test/api";
 // export const BASE_API = "http://127.0.0.1:8000/api";
 export const BASE_API = "https://node.directwebhost.com/api";
+export const NODE_SERVER = "http://localhost:3002";
 
 // DEFAULT ROOT PATH
 export const DEFAULT_PATH = PATH_DASHBOARD.general.app; // as '/app'
--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,6 @@
 import { io } from "socket.io-client";
+import { NODE_SERVER } from "./config";
 
-export const socket = io("http://localhost:3002", {
+export const socket = io(NODE_SERVER, {
     autoConnect: true
   });
